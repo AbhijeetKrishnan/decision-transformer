725370abaaf887fec2278b81939b4b1622a4f434
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2023-11-30 20:41:19 -0500

    Fix submodules

--- karel/karel_reward.py
+++ karel/karel_reward.py
@@ -4,6 +4,6 @@ from leaps.prl_gym.exec_env import ExecEnv2
 def karel_reward(program_text, mdp_config=None):
     program = program_text.replace('\\', '').replace('\'', '')
     
-    karel_env = ExecEnv2(mdp_config)
+    karel_env = ExecEnv2(mdp_config['args'])
     reward, pred_program = karel_env.reward(program, is_program_str=True)
     return reward
